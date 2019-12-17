ae4da28936c2d0dd7641d6af7d39d13cf5fe1636
Summer Rizzo <srainrizzo@gmail.com>
2019-12-17 10:47:30 -0500

    Code 1 Challenge - Deliverables Done

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,14 +1,32 @@
 class Band
-    attr_accessor :name
-    attr_reader :hometown
+
+    attr_reader :hometown, :name
+
     @@all = []
 
     def initialize(name, hometown)
         @name = name
         @hometown = hometown
+
         @@all << self
     end
 
+    ## INSTANCE METHODS ##
+    
+    def concerts
+        Concert.all.select{|concert| concert.band == self}
+    end
+
+    def play_in_venue(date, venue)
+        Concert.new(date, self, venue)
+    end
+
+    def all_introductions
+        concerts.map{|concert| "Hello #{concert.city}!!!!!, we are #{self.name} and we're from #{self.hometown}"}
+    end
+
+
+    ## CLASS METHODS ##
     def self.all
         @@all
     end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,13 +1,49 @@
 class Venue
-    attr_accessor :title
-    attr_reader :city
+
+    attr_reader :city, :title
+
     @@all = []
 
     def initialize(title, city)
         @title = title
         @city = city
+
         @@all << self
     end
+
+    ## INSTANCE METHODS ##
+
+    def concerts
+        Concert.all.select{|concert| concert.venue == self}
+    end
+
+    def bands
+        concerts.map{|concert| concert.band}
+    end
+
+    def concert_on(date)
+        concerts.find{|concert| concert.date == date}
+    end
+
+    def most_frequent_band
+        ## NOTE ##
+        # I wanted to use group_by for this, but I got stressed and didn't want to risk too much time on it.
+        # I know this is a little clunkier, but hey, if it works it works!
+
+        ## Create empty hash to keep a count of how many times a band played a concert in this instance of Venue
+        ## Iterate through the concerts. If key of band already exists, add 1 to the value. If not, initialize it with a start of 1
+        band_hash = {}
+        concerts.each {|concert|
+            if band_hash[concert.band]
+                band_hash[concert.band] += 1
+            else
+                band_hash[concert.band] = 1
+            end
+        }
+        band_hash.max_by{|band, frequency| frequency}[0]
+    end
+
+    ## CLASS METHODS ##
 
     def self.all
         @@all
