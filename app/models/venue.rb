0a7483e8ed4041cb48beb357d085c6c752933232
hylobates-lar <alison.v.quaglia@gmail.com>
2019-12-17 10:46:46 -0500

    deliverables done

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -12,4 +12,38 @@ class Venue
     def self.all
         @@all
     end
+
+    def concerts
+        Concert.all.select do |concert_instance|
+            concert_instance.venue == self 
+        end
+    end
+
+    def bands
+        self.concerts.map do |concert_instance|
+            concert_instance.band 
+        end
+    end
+
+    def concert_on(date)
+        Concert.all.find do |concert_instance|
+            concert_instance.date == date
+        end
+    end
+
+    def most_frequent_band
+        band_visits = []
+        Concert.all.map do |concert_instance|
+            if concert_instance.venue == self
+                band_visits << concert_instance.band 
+            end
+        end
+        band_visits.max_by do |band|   
+        end 
+    end
 end
+
+
+
+
+
