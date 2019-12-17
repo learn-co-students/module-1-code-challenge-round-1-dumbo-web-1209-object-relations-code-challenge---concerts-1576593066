0af5b3ce5125cceb730fc4d00bb835a4f23451cb
tdonovan79 <tdonovan79@gmail.com>
2019-12-17 10:46:03 -0500

    deliverable done

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,7 @@
 class Band
-    attr_accessor :name
+
     attr_reader :hometown
+    attr_accessor :name
     @@all = []
 
     def initialize(name, hometown)
@@ -11,5 +12,17 @@ class Band
 
     def self.all
         @@all
+    end
+    # should return an array of all the concerts that the band has played in
+    def concerts
+        Concert.all.select{|concert_instance| concert_instance.band == self}
+    end
+    # takes a venue and date (as a string) as arguments, and creates a new concert for the band in that venue on that date
+    def play_in_venue(new_venue, new_date)
+        Concert.new(new_date, self, new_venue)
+    end
+    # returns an array of strings representing all the introductions for this band
+    def all_introductions
+        self.concerts.map(&:introduction)
     end
 end
