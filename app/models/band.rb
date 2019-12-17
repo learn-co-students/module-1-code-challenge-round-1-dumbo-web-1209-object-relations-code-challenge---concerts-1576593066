7e3557d4e024c4b4ebc4b68c415a875d777b11ac
Annie Liao <liaowow@gmail.com>
2019-12-17 10:45:44 -0500

    deliverables done

--- app/models/band.rb
+++ app/models/band.rb
@@ -9,6 +9,24 @@ class Band
         @@all << self
     end
 
+    # return an array of all the concerts that the band has played in
+    def concerts
+        Concert.all.select do |concert_instance|
+            concert_instance.band == self
+        end
+    end
+
+    def play_in_venue(venue, date)
+        Concert.new(date, self, venue)
+    end
+
+    # returns an array of strings representing all the introductions for this band
+    def all_introductions
+        # loop thru Concert class and find matching band names by comparing concert's band name
+        # if concert class band is this band, return that instance's intro 
+        Concert.all.map{|concert_instance| concert_instance.introduction if concert_instance.band == self}.uniq.compact
+    end
+
     def self.all
         @@all
     end
