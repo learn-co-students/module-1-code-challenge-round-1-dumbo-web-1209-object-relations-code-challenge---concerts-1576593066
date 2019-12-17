559d9f9386674b16be82e18f23f3a8366c9581a1
mk17173n <Monsur.Khan@gmail.com>
2019-12-17 10:47:47 -0500

    done

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -12,4 +12,32 @@ class Venue
     def self.all
         @@all
     end
+
+    def concerts
+        Venue.all do |venue_instance|
+            venue_instance.concert
+        end
+    end
+
+    def bands
+        Venue.all do |venue_instance|
+            venue_instance.band 
+        end
+    end
+
+
+
+    def concert_on(date)
+        Concert.all.find do |concert_instance|
+            concert.date === date
+        end
+    end
+
+    def most_frequent_band
+       mvb=Venue.all.sort_by do |venue_instance|
+            venue_instance.band
+        end
+        mvb.last
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+b1 = Band.new("flame", "queens")
+b2 = Band.new("ice", "brooklyn")
+b3 = Band.new("ice", "manhattan")
+
+v1 = Venue.new("radio city", "manhattan")
+v2 = Venue.new("palace", "manhattan")
+v3 = Venue.new("hammerstein", "manhattan")
+
+c1 = Concert.new("12/2/2019", b1, v1)
+c2 = Concert.new("12/3/2019", b2, v2)
+c3 = Concert.new("12/4/2019", b3, v2)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
