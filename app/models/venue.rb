8c3d1b3635b3196087ec4ab4c96a126da125f5e7
teapuddles <kingsley.gleeson@gmail.com>
2019-12-17 10:46:37 -0500

    :(

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,15 +1,30 @@
 class Venue
     attr_accessor :title
     attr_reader :city
+
     @@all = []
 
     def initialize(title, city)
         @title = title
         @city = city
+
         @@all << self
     end
 
     def self.all
         @@all
     end
+
+    def concerts
+        Concert.all.select do |concert_instnace|
+            concert_instnace.venue.title == self.title
+        end
+    end
+
+    def bands
+        concerts.map do |concert|
+            concert.name
+        end
+    end
+
 end
